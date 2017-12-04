b1c150be7655114af3ffa8b1c12c39421ef8d60c
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2017-12-04 12:20:43 +0100

    Solution day 4, extract solution 2 from 1

---
+++ 04_day/2_solution.py
@@ -0,0 +1,31 @@
+f = open('my.input', 'r')
+
+def isAnagram(w1,w2):
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    for l in letters:
+        if w1.count(l) != w2.count(l):
+            return False
+    print(w1, w2)
+    return True
+
+count = 0
+for line in f:
+    
+    words = line[:-1].split(' ')
+
+    seen = set()
+    valid = True;
+
+    for word in words:
+        for s in seen:
+            if isAnagram(s,word):
+                valid = False
+
+        if word in seen:
+            valid = False;
+        else:
+            seen.add(word)
+    
+    if valid:
+        count +=1
+print(count)
