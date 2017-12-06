634775717e70ad6808d1981e567aab6655cbb258
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2017-12-06 12:16:33 +0100

    minimize day 06 solution

--- 06_day/solution.py
+++ 06_day/solution.py
@@ -2,16 +2,13 @@ my_input = "2   8   8   5   4   2   3   1   5   5   1   2   15  13  5   14"
 
 banks = [int(x) for x in my_input.split(' ') if len(x) > 0]
 states = []
-count = 0 
+
 while True:
     if str(banks) in states:
-        print('Duplicate found at count:', count)
+        print('Duplicate found at count:', len(states))
         start = states.index(str(banks))
         end = len(states)
-        print('Index of duplicate:', start)
-        print('Length:', end)
         print('Loop length:', end - start)
-        
         exit()
         
     states.append(str(banks))
@@ -23,9 +20,3 @@ while True:
 
     for i in range(1, value+1):
         banks[ (idx + i) % len(banks) ] += 1
-
-    count += 1
-
-print(my_input)
-
-
