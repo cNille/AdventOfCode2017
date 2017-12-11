f4e1d8a6085278b2aa1da05f3af2edfdf827971a
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2017-12-11 23:19:30 +0100

    Solution 1&2 day 08 in python

---
+++ 08_day/2_solution.py
@@ -0,0 +1,33 @@
+import re
+
+data = open('my.input')
+
+var = {}
+max_val = 0
+
+for row in data:
+    [   variable, 
+        operator, 
+        delta, 
+        iif, 
+        condition_variable, 
+        condition_operator, 
+        condition_value] = row[:-1].split(' ')
+    
+    if condition_variable not in var:
+        exec('var["' + condition_variable + '"] = 0')
+    if variable not in var: 
+        exec('var["' + variable + '"] = 0')
+
+    if not eval('var["' + condition_variable + '"] ' + condition_operator + ' ' + condition_value):
+        continue
+
+    if operator == 'dec':
+        exec('var["' + variable + '"] -= ' + delta)
+    else:     
+        exec('var["' + variable + '"] += ' + delta)
+
+    for v in var.values():
+        max_val = max(max_val, v)
+
+print(max_val)
