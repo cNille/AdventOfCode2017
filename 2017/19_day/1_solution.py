01bb46112e511f0ab3de3bb1c8b62513ad3e9afd
Christopher_Nilsson <christopher.nille.nilsson@gmail.com>
2017-12-23 22:51:59 +0100

    solution 1 & 2 day 19

---
+++ 2017/19_day/1_solution.py
@@ -0,0 +1,41 @@
+import math
+
+road = open('my.input').read().split('\n')
+road = [list(x) for x in road]
+letters = []
+delta = 1
+going_vertical = True
+
+for idx,item in enumerate(road[0]):
+    if item == "|":
+        [x, y] = (0, idx)
+        break
+
+while True:
+    if going_vertical:
+        x += delta
+    else:
+        y += delta
+    
+    if road[x][y] == ' ':
+        print('Road ended at',x,y)
+        break
+
+    if road[x][y] == '+':
+        if going_vertical:
+            if road[x][y-1] == '-':
+               delta = -1 
+            if road[x][y+1] == '-':
+               delta = 1 
+        else:
+            if x-1 > 1 and road[x-1][y] == '|':
+               delta = -1 
+            if x+1 < len(road) - 1 and road[x+1][y] == '|':
+               delta = 1 
+        going_vertical = not going_vertical
+        continue
+        
+    if road[x][y] != '|' and road[x][y] != '-':
+        letters.append(road[x][y])
+
+print('Letters', ''.join(letters))
