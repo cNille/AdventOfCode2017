092b97e525c2da56b173a1778ffcfabf06ec201e
Christopher Nilsson <chrn@netlight.com>
2018-12-10 12:50:53 +0100

    solved day 10

--- 2018/day_10.py
+++ 2018/day_10.py
@@ -1,10 +1,8 @@
+import matplotlib.pyplot as plt
+import sys
+
 # Import data
 data = [x.strip() for x in open('day_10_input.txt','r').readlines()]
-
-min_x = 10000
-min_y = 10000
-max_x = 10000
-max_y = 10000
 
 points = []
 for line in data:
@@ -12,69 +10,41 @@ for line in data:
     p_y = int(line[18:24])
     v_x = int(line[36:38])
     v_y = int(line[40:42])
-    min_x = min(p_x, min_x)
-    min_y = min(p_y, min_y)
-    max_x = max(p_x, max_x)
-    max_y = max(p_y, max_y)
     points.append([p_x, p_y, v_x, v_y])
 
+graph_size = [sys.maxint, sys.maxint]
+getting_smaller = True
+iteration = 0
+while getting_smaller:
+    min_x = sys.maxint
+    min_y = sys.maxint
+    max_x = 0
+    max_y = 0
+    for i in range(len(points)):
+        points[i][0] += points[i][2]
+        points[i][1] += points[i][3]
+        min_x = min(points[i][0], min_x)
+        min_y = min(points[i][1], min_y)
+        max_x = max(points[i][0], max_x)
+        max_y = max(points[i][1], max_y)
 
-print('MinMax', min_x, min_y, max_x, max_y)
+    delta_x = abs(max_x - min_x)
+    delta_y = abs(max_y - min_y)
 
-for i in range(10):
-    print(points[i])
+    getting_smaller = delta_x <= graph_size[0] and delta_y <= graph_size[1]
+    graph_size[0] = delta_x
+    graph_size[1] = delta_y
 
-print('Type "y" to get next...')
-# y = ''
-# nxt = input()
-# print(nxt, ':', y)
-# while nxt == 'y':
-import matplotlib.pyplot as plt
+# Part 2
+print('Iteration : %d' % (iteration-1))
 
-curr= 0
-
-graph_size = [99999,99999]
-
-while curr < 20000000:
-    # Plot on graph
-
-    if curr > 10100 :
-        print(curr)
-        x = [p[0] for p in points]
-        y = [p[1] for p in points]
-        plt.scatter(x, y)
-        plt.title("Scatter Plot")
-        plt.ylim(150,250)
-        plt.show(block=True)
-        plt.pause(0.1)
-        plt.close()
-        for i in range(len(points)):
-            points[i][0] += points[i][2] * 0.1
-            points[i][1] += points[i][3] * 0.1
-
-    else:
-        for i in range(len(points)):
-            points[i][0] += points[i][2]
-            points[i][1] += points[i][3]
-
-            for p in points:
-                min_x = min(p[0], min_x)
-                min_y = min(p[1], min_y)
-                max_x = max(p[0], max_x)
-                max_y = max(p[1], max_y)
-            delta_x = max_x - min_x
-            delta_y = max_y - min_Y
-
-            getting_smaller = delta_x < graph_size[0] and delta_y < graph_size[1]
-
-    curr += 1
-    #nxt = input()
-
-# Wrong: GPEPPEJ
-# Wrong: JEPPEPG
-# Wrong: JEPPPEPG
-# Correct: GPEPPPEJ
-
-# Part 2:
-# Wrong: 10100
-# Test: 10101
+# Part 1
+for i in range(len(points)):
+    points[i][0] -= points[i][2]
+    points[i][1] -= points[i][3]
+x = [p[0] for p in points]
+y = [p[1] for p in points]
+plt.scatter(x, y)
+plt.title("Scatter Plot")
+plt.ylim(150,250)
+plt.show(block=True)
