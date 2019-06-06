4f92ac315937fbf37cf60f0a86cdf78300215f42
Christopher Nilsson <christopher.nille.nilsson@gmail.com>
2019-06-06 19:52:24 +0200

    solve day 23 part 2

---
+++ 2018/23_2.py
@@ -0,0 +1,43 @@
+import re
+# Part 2
+
+# Load data
+DATA = [line.strip() for line in open('23.in', 'r').readlines()]
+
+# Generate Bots
+bots = []
+for line in DATA:
+    # Line format: "pos=<-33124555,22143443,77789621>, r=85920143"
+    s = line.split("=")
+    x,y,z = s[1][1:-4].split(',')
+    r = s[2]
+    b = (x,y,z,r)
+    bots.append(map(int, b))
+
+# Alternatively we could use priorityqueue
+queue = []
+for x,y,z,r in bots:
+    center = abs(x) + abs(y) + abs(z)
+    lower_bound = center - r
+    upper_bound = center + r + 1
+
+    queue.append((max(0, lower_bound), 1))
+    queue.append((upper_bound, -1))
+
+# Sort bounds according to distance.
+queue.sort(key= lambda x: x[0])
+
+count = 0
+maxCount = 0
+result = 0
+for q in queue:
+    distance, bound = q
+
+    # Decrease for lower_bound, increment for upper_bound
+    count += bound
+
+    if count > maxCount:
+        # New max found
+        result = distance
+        maxCount = count
+print(result)
