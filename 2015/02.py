5d7fd76343e06d60f60881fab1783b1639343bc7
Christopher Nilsson <christopher.nille.nilsson@gmail.com>
2019-09-04 16:49:09 +0200

    2015 day 2 solution part 1

---
+++ 2015/02.py
@@ -0,0 +1,15 @@
+data = open('02.input').readlines()
+
+# Part 1:
+paper = 0
+sizes = [
+    map(int, d[:-1].split('x'))
+    for d
+    in data
+]
+for (w, l, h) in sizes:
+    m = min(w*l,l*h,h*w)
+    paper += 2*l*w + 2*w*h + 2*h*l + min(w*l,l*h,h*w)
+print("Result part 1: %d" % paper)
+
+# Part 2:
