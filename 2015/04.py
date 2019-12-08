2ddefebcfdd43740f99b5e5b3e52bb4757bbd248
Christopher Nilsson <christopher.nille.nilsson@gmail.com>
2019-12-08 20:12:11 +0100

    fix day 4 2015

--- 2015/04.py
+++ 2015/04.py
@@ -1,33 +1,23 @@
 import hashlib
-
 data = "ckczppom"
-# Part 1:
 
+# Part 1:
 nbr = 1
 while True:
     h = hashlib.md5(("%s%d" % (data, nbr)).encode())
     h = h.hexdigest()
     if h[:5] == "00000":
         break
-    if nbr % 100000 == 0:
-        print("Iteration %d" % nbr)
     nbr += 1
-
 print("Solution part 1: %d" % nbr)
-
-# incorrect: ckczppom117946
 
 # Part 2:
-
-nbr = 386700000
-
+nbr = 503000000 
+nbr = 0
 while True:
-    h = hashlib.md5(("%s%d" % (data, nbr)).encode())
-    h = h.hexdigest()
-    if h[:5] == "000000":
+    h = hashlib.md5(("%s%d" % (data, nbr)).encode()).hexdigest()
+    if h[:6] == "000000":
         break
-    if nbr % 100000 == 0:
-        print("Iteration %d" % nbr)
     nbr += 1
 
-print("Solution part 1: %d" % nbr)
+print("Solution part 2: %d" % nbr)
