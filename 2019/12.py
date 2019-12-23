f8b13fea3ec35ab0e146b698811ad2c4eafe696f
Christopher Nilsson <christopher.nille.nilsson@gmail.com>
2019-12-23 09:31:02 +0100

    solve day 12 2019

--- 2019/12.py
+++ 2019/12.py
@@ -1,30 +1,18 @@
 import re
+import numpy as np
 f = open('12.input', 'r')
 
 # Test
 content = [x.strip() for x in f.readlines()]
-content = [ '<x=-8, y=-10, z=0>', '<x=5, y=5, z=10>', '<x=2, y=-7, z=3>', '<x=9, y=-8, z=-3>' ]
-content = [ '<x=-1, y=0, z=2>', '<x=2, y=-10, z=-7>', '<x=4, y=-8, z=8>', '<x=3, y=5, z=-1>' ]
-
-def printpoints(points, step):
-    print(step)
-    for p in points:
-        print(p)
 
 def energy(points):
     kinetic = [sum(map(abs,x[:3])) for x in points]
+    kin = sum(map(abs,points[1][:3]))
     potential = [sum(map(abs,x[3:])) for x in points]
+    pot = sum(map(abs,points[1][3:]))
     total = sum([ k*p for k,p in zip(kinetic, potential)])
-    #print(kinetic, potential, sum(kinetic), sum(potential), total)
-    print(sum(kinetic), sum(potential), total, points)
+    tot = pot * kin
     return total
-    #return sum(
-    #    [ 
-    #        sum(map(abs,x[:3])) * sum(map(abs,x[3:]))
-    #        for x
-    #        in points
-    #    ]
-    #)
 
 initial_points = []
 for c in content:
@@ -32,8 +20,11 @@ for c in content:
     initial_points.append((int(x), int(y), int(z), 0,0,0)) 
 points = initial_points
 
-
+i = 0
 def round(points):
+    global i
+    i +=1
+    # Calculate new velocity
     new_points = []
     for m1 in points:
         dx, dy, dz = 0,0,0
@@ -55,6 +46,7 @@ def round(points):
         new_points.append(new_point)
     points = new_points
 
+    # Calculate new position
     new_points = []
     for m1 in points:
         new_point = (
@@ -66,76 +58,50 @@ def round(points):
             m1[5],
         )
         new_points.append(new_point)
-    points = new_points
-    #printpoints(points, i+1)
-    return points
+    return new_points
 
-# origin = (0,0,0)
-# def round_smart(points):
-#     new_points = []
-#     for m1 in points:
-#         dx, dy, dz = 0,0,0
-#         for m2 in points:
-#             if m1 == m2:
-#                 continue
-#             dx = m2[0] - m1[0]
-#             dy = m2[1] - m1[1]
-#             dz = m2[2] - m1[2]
-# 
-#         new_point = (
-#             m1[0] + dx ,
-#             m1[1],
-#             m1[2],
-#             m1[3] + dx,
-#             m1[4] + dy,
-#             m1[5] + dz,
-#         )
-#         new_points.append(new_point)
-#     points = new_points
-#     #printpoints(points, i+1)
-#     return points
-        
 
-energy(points)
-for i in range(2800):
-    print(i)
+for i in range(1000):
     points = round(points)
     energy(points)
-#print("Part 1:", energy(points))
-exit()
+print "Part 1: %d" % energy(points)
 
-def get_pos(p):
-    return [x[:3] for x in p]
-def get_vel(p):
-    return [x[3:] for x in p]
-def is_zero(points):
-    vel = get_vel(points)
-    for x,y,z in vel:
-        if x != 0 or y != 0 or z != 0:
-            return False
-    return True
-        
-def equalstart(points):
-    for i, p in enumerate(points):
-        if p != initial_points[i]:
-            return False
-    return True
-
-
-print("Points", points)
-
-
+energy(points)
 points = round(points)
-count = 1
+energy(points)
+count = 0
+x_round = None
+y_round = None
+z_round = None
+xseen = None
+yseen = None
+zseen = None
 
-count, points = (283000000, [(511, 116, -260, 23, 16, -17), (-39, 196, -361, 17, 34, -4), (-99, -217, 846, 14, -24, 28), (-367, -69, -222, -54, -26, -7)]) 
-
-while not equalstart(points):
-    count += 1
+while True:
     points = round(points)
-    print(count, energy(points))
-    if count % 1000000 == 0:
-        print(count, points, energy(points))
-points = round(points)
-print("should be same", points)
+    x = tuple((x[0], x[3]) for x in points)
+    if x == xseen:
+        if x_round == None:
+            x_round = count 
+    elif xseen == None:
+            xseen = x
 
+    y = tuple((y[1], y[4]) for y in points)
+    if y == yseen:
+        if y_round == None:
+            y_round = count
+    elif yseen == None:
+            yseen = y
+
+    z = tuple((z[2], z[5]) for z in points)
+    if z == zseen:
+        if z_round == None:
+            z_round = count
+    elif zseen == None:
+            zseen = z
+
+    if x_round != None and y_round != None and z_round != None:
+        res = np.lcm.reduce([x_round, y_round, z_round])
+        print "Part 2: %d " % res
+        exit()
+    count += 1
