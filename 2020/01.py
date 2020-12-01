7c764cb0b68e438d29659e63d26cfc45a998c9c9
Christopher Nilsson <christopher@sanalabs.com>
2020-12-01 08:43:35 +0100

    prettify 2020 01

--- 2020/01.py
+++ 2020/01.py
@@ -1,40 +1,77 @@
-from itertools import permutations
-f = open('test2', 'r')
+f = open('01.test', 'r')
 f = open('01.input', 'r')
-
 content = [int(x.strip()) for x in f.readlines()]
 
+# O(n^2)
+def part1slow(arr):
+    for x in arr:
+        for y in arr:
+            if x + y == 2020:
+                return x * y
 
+# O(n)
 def part1(arr):
-    rests = {}
+    rests = set()
 
     for x in arr:
         rest = 2020 - x
-        if x in rests:
+        if rest in rests:
             return rest * x
-        else:
-            rests[rest] = True 
-    return "not found"
+        else: 
+            rests.add(x)
 
+print("Part 1 solution: %d" % part1(content))
 
-#print(part1(content))
+# O(n^3)
+def part2slow(arr):
+    for x in arr:
+        for y in arr:
+            for z in arr:
+                if x + y + z == 2020:
+                    return x * y * z
 
-perm = permutations(content, 2)
-newList = []
-
-for x in perm:
-    newList.append((x[0]+x[1], x))
-
-def part2(arr1, arr2):
+# O(n^2 + n) => O(n^2)
+def part2(arr):
     rests = {}
-    for x in arr1:
-        rests[x[0]] = x[1]
+    for x in arr:
+        for y in arr:
+            rests[x + y] = x * y
 
-    for x in arr2:
+    for x in arr:
         rest = 2020 - x
         if rest in rests:
-            print(x, rests[rest][0] , rests[rest][1])
-            print(x * rests[rest][0] * rests[rest][1])
-            return rest * x
-    return "not found"
-print(part2(newList, content))
+            return x * rests[rest]
+
+print("Part 2 solution: %d" % part2(content))
+
+# def part1(arr):
+#     rests = {}
+#     for x in arr:
+#         rest = 2020 - x
+#         if x in rests:
+#             return rest * x
+#         else:
+#             rests[rest] = True 
+#     return "not found"
+# 
+# 
+# print("Part 1: %d" % part1(content))
+# 
+# from itertools import permutations
+# perm = permutations(content, 2)
+# newList = []
+# 
+# for x in perm:
+#     newList.append((x[0]+x[1], x))
+# 
+# def part2(arr1, arr2):
+#     rests = {}
+#     for x in arr1:
+#         rests[x[0]] = x[1]
+# 
+#     for x in arr2:
+#         rest = 2020 - x
+#         if rest in rests:
+#             return (x * rests[rest][0] * rests[rest][1])
+#     return "not found"
+# print("Part 2: %d" % part2(newList, content))
