25e039ef6fe2a7e1b56519efdfe5ea19d1054372
Christopher Nilsson <christopher@sanalabs.com>
2020-12-06 14:27:13 +0100

    aoeu

--- 2020/06.py
+++ 2020/06.py
@@ -35,6 +35,37 @@ print "Solution part 2: %d" % part2(groups)
 
 # Alternative solutions
 
+def part1set(groups):
+    sets = [map(set, group.splitlines()) for group in groups]
+    unions = [set.union(*s) for s in sets]
+    return sum(map(len, unions))
+print part1set(groups)
+
+def part2set(groups):
+    sets = [map(set, group.splitlines()) for group in groups]
+    intersections = [set.intersection(*s) for s in sets]
+    return sum(map(len, intersections))
+print part2set(groups)
+
+def part2(groups):
+    total = 0
+    for group in groups:
+        # Use dictionary to count occurrences of letters
+        occurrences = {}
+        for letter in group:
+            if letter not in occurrences:
+                occurrences[letter] = 0
+            occurrences[letter] +=1 
+
+        # Amount of persons in the group will be one more than new-lines.
+        person_count = occurrences['\n'] + 1 if '\n' in occurrences else 1 
+
+        # Count all letters that occur same amount as persons in group
+        all_answers = [x for x in occurrences.values() if x == person_count]
+        total += len(all_answers)
+    return total
+
+
 from collections import Counter
 def part2count(groups):
     total = 0
@@ -44,9 +75,6 @@ def part2count(groups):
         all_answers = [x for x in letter_counter.values() if x == person_count]
         total += len(all_answers)
     return total
-
-print part2count(groups)
-
 
 def part1func(groups):
     return sum([
