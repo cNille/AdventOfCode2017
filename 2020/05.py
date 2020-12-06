8eb5187e4e006d4819d9c5095b7da59548d928c1
Christopher Nilsson <christopher@sanalabs.com>
2020-12-06 06:15:01 +0100

    fix: cleanup

--- 2020/05.py
+++ 2020/05.py
@@ -2,26 +2,47 @@ import re
 f = open('05.input', 'r')
 data = [x.strip() for x in f.readlines()]
 
+def calc_id(seat):
+    transformation= { 'F': '0', 'B': '1', 'L': '0', 'R': '1'}
+    binary = "".join([transformation[x] for x in seat])
+    return int(binary,2)
+
+def part1binary(data):
+    ids = [calc_id(x) for x in data]
+    return max(ids)
+
+def part2binary(data):
+    ids = [calc_id(x) for x in data]
+
+    for i in range(max(ids)):
+        low = i - 1
+        high = i + 1
+        if  low in ids and high in ids and i not in ids:
+            return i
+
+print "Part 1 binary solution: %d " % part1binary(data)
+print "Part 2 binary solution: %d " % part2binary(data)
+
 def part1(data):
-    max_id = 0
+    ids = []
     for line in data:
         current = 0
         row = 64
-        for c in line[:7]:
-            if c == 'B':
+        for letter in line[:7]:
+            if letter == 'B':
                 current += row
-            row = row / 2
+            row = row >> 1 # Shift right 1 bit
 
-        current = current * 8
+        current = current << 3 # Shift left 3 bits
+
         row = 4 
         for c in line[7:]:
             if c == 'R':
                 current += row
-            row = row / 2
-        max_id = current if current > max_id else max_id
-        if current > 937:
-            print line
-    return max_id
+            row = row >> 1 # Shift right 1 bit
+        ids.append(current)
+
+    return max(ids)
 
 
 def part2(data):
@@ -34,14 +55,14 @@ def part2(data):
         for c in line[:7]:
             if c == 'B':
                 current += row
-            row = row / 2
+            row = row >> 1 # Shift right 1 bit
 
-        current = current * 8
+        current = current << 3 # Shift left 3 bits
         row = 4 
         for c in line[7:]:
             if c == 'R':
                 current += row
-            row = row / 2
+            row = row >> 1 # Shift right 1 bit
 
         id_list.append(current)
     id_list = sorted(id_list)
@@ -51,9 +72,27 @@ def part2(data):
         if  low in id_list and high in id_list and i not in id_list:
             return i
 
-d = sorted(data)
-for l in d[:5]:
-    print l
 print "Part 1 solution: %d " % part1(data)
-exit()
 print "Part 2 solution: %d " % part2(data)
+
+def part1sorting(data):
+    normalized = []
+    for line in data:
+        new_line = line.replace('R','B').replace('L','F')
+        normalized.append(new_line)
+    d = sorted(normalized)
+    return d[0]
+
+print "Part 1 sorted solution: %s " % part1sorting(data)
+
+def part1str(data):
+    d = sorted(data)
+    highest_row_value = d[0][:7]
+    i = 1
+    while d[i][:7] == highest_row_value:
+        i+=1
+
+    highest_value = d[i-1]
+    return calc_id(highest_value)
+
+print "Part 1 string solution: %d " % part1str(data)
